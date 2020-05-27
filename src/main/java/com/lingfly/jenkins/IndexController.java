1ee6d04f688c4506a44c432addd165c551d299a2
lingfly <zhengzhongwei0@gmail.com>
2020-05-27 16:35:38 +0800

    使用独立tomcat

--- src/main/java/com/lingfly/jenkins/IndexController.java
+++ src/main/java/com/lingfly/jenkins/IndexController.java
@@ -1,11 +1,18 @@
 package com.lingfly.jenkins;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class IndexController {
+public class IndexController extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(IndexController.class);
+    }
+
     @RequestMapping("/test")
     @ResponseBody
     public String test(){
